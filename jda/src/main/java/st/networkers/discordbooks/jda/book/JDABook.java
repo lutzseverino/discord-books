b48031954ce93af5ef23fac6686a4b78e64dcc84
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-04-04 20:11:16 +0200

    Add JDABook implementation

---
+++ jda/src/main/java/st/networkers/discordbooks/jda/book/JDABook.java
@@ -0,0 +1,35 @@
+package st.networkers.discordbooks.jda.book;
+
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
+import st.networkers.discordbooks.book.Book;
+
+public class JDABook extends Book {
+
+    public JDABook(String name) {
+        super(name);
+    }
+
+    /**
+     * Sends the first book page to a specified
+     * channel.
+     *
+     * @param channel the Discord message channel to send the book to
+     */
+    public void sendBook(MessageChannel channel) {
+        sendBook(channel, 0);
+    }
+
+    /**
+     * Sends the specified book page to the provided
+     * channel.
+     *
+     * @param channel the Discord message channel to send the book to
+     * @param index   the page number to send
+     */
+    public void sendBook(MessageChannel channel, int index) {
+        pages.get(index).getContent().send();
+    }
+}
