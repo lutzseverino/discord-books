e68f073d726066ae13254d44f9c503e2f8fc3d47
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-04-06 00:55:56 +0200

    Make PageCache store pages in a map

--- core/src/main/java/st/networkers/discordbooks/cache/PageCache.java
+++ core/src/main/java/st/networkers/discordbooks/cache/PageCache.java
@@ -2,29 +2,29 @@ package st.networkers.discordbooks.cache;
 
 import st.networkers.discordbooks.book.Book;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PageCache implements Cache<Book.Page> {
-    private final List<Book.Page> pages = new ArrayList<>();
+    private final Map<String, Book.Page> pages = new HashMap<>();
 
     @Override public void add(Book.Page page) {
-        pages.add(page);
+        pages.put(page.getId(), page);
     }
 
     @Override public Book.Page get(String id) {
-        return null;
+        return pages.get(id);
     }
 
     @Override public Iterable<Book.Page> getAll() {
-        return pages;
+        return pages.values();
     }
 
-    @Override public void remove(String name) {
-
+    @Override public void remove(String id) {
+        pages.remove(id);
     }
 
     @Override public void remove(Book.Page page) {
-
+        pages.remove(page.getId());
     }
 }
