05adff228ab051a01279599428319ed579d3cce0
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-04-07 23:20:51 +0200

    Create JDABooks#sendBook()

--- jda/src/main/java/st/networkers/discordbooks/jda/JDABooks.java
+++ jda/src/main/java/st/networkers/discordbooks/jda/JDABooks.java
@@ -1,8 +1,11 @@
 package st.networkers.discordbooks.jda;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.MessageChannel;
 import org.jetbrains.annotations.NotNull;
 import st.networkers.discordbooks.DiscordBooks;
+import st.networkers.discordbooks.book.Book;
+import st.networkers.discordbooks.jda.book.JDABook;
 import st.networkers.discordbooks.jda.listeners.ButtonListener;
 
 public class JDABooks extends DiscordBooks {
@@ -12,5 +15,27 @@ public class JDABooks extends DiscordBooks {
     }
 
     public JDABooks() {
+    }
+
+    /**
+     * @param name    the name of the book
+     * @param channel the channel to send the book to
+     */
+    public void sendBook(String name, MessageChannel channel) {
+        sendBook(name, channel, 0);
+    }
+
+    /**
+     * @param name    the name of the book
+     * @param channel the channel to send the book to
+     * @param index   the index of the page to send
+     * @throws IllegalArgumentException if the Book is not a JDABook
+     */
+    public void sendBook(String name, MessageChannel channel, int index) {
+        Book book = getBooks().get(name);
+
+        if (book instanceof JDABook)
+            ((JDABook) book).send(channel, index);
+        else throw new IllegalArgumentException("Book is not a JDABook");
     }
 }
