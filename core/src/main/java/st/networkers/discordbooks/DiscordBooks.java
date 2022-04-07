a02d5b647328fbd89d29e164cc052e366b7b3b2a
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-04-07 23:18:16 +0200

    Add documentation

--- core/src/main/java/st/networkers/discordbooks/DiscordBooks.java
+++ core/src/main/java/st/networkers/discordbooks/DiscordBooks.java
@@ -10,6 +10,11 @@ public class DiscordBooks {
     protected final Cache<Book> books = new BookCache();
     protected final Cache<Book.Page> pages = new PageCache();
 
+    /**
+     * Registers new books to the cache.
+     *
+     * @param books the books to add
+     */
     public void addBooks(@NotNull Book... books) {
         for (Book book : books) {
             this.books.add(book);
@@ -17,18 +22,33 @@ public class DiscordBooks {
         }
     }
 
+    /**
+     * Registers a new book to the cache.
+     *
+     * @param book the book to add
+     */
     public void addBook(Book book) {
         addBooks(book);
     }
 
+    /**
+     * @param name the name of the book
+     * @return the book with the given name
+     */
     public Book getBook(String name) {
         return books.get(name);
     }
 
+    /**
+     * @return the book cache
+     */
     public Cache<Book> getBooks() {
         return books;
     }
 
+    /**
+     * @return the page cache
+     */
     public Cache<Book.Page> getAllPages() {
         return pages;
     }
