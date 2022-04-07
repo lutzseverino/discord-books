60b306728d5b0f538181763d02f26f0d9dd429f8
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-04-08 00:30:53 +0200

    Add default buttons Cleanup

--- jda/src/main/java/st/networkers/discordbooks/jda/book/JDABook.java
+++ jda/src/main/java/st/networkers/discordbooks/jda/book/JDABook.java
@@ -14,12 +14,11 @@ import st.networkers.discordbooks.message.Sendable;
 
 import javax.annotation.CheckReturnValue;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class JDABook extends Book {
-    private Button previousButton;
-    private Button nextButton;
+    private Button previousButton = new ButtonImpl(null, "Previous", ButtonStyle.PRIMARY, true, null);
+    private Button nextButton = new ButtonImpl(null, "Next", ButtonStyle.PRIMARY, true, null);
     private final List<Button> buttons = new ArrayList<>();
     private final List<ActionRow> actionRows = new ArrayList<>();
 
@@ -47,14 +46,12 @@ public class JDABook extends Book {
     public void send(MessageChannel channel, int index) {
         Sendable<?> sendable = pages.get(index).getContent();
 
-        if (sendable instanceof JDAMessage) {
-            JDAMessage jdaMessage = (JDAMessage) sendable;
-            applyActionRows(channel.sendMessage(jdaMessage.getMessage()), index).queue();
-        } else if (sendable instanceof JDAMessageEmbed) {
-            JDAMessageEmbed jdaMessageEmbed = (JDAMessageEmbed) sendable;
-            applyActionRows(channel.sendMessageEmbeds(Arrays.asList(jdaMessageEmbed.getMessage())), index).queue();
-        } else
-            throw new IllegalArgumentException("Book's Sendable must be a JDAMessage or JDAMessageEmbed");
+        if (sendable instanceof JDAMessage)
+            applyActionRows(channel.sendMessage(((JDAMessage) sendable).getMessage()), index).queue();
+        else if (sendable instanceof JDAMessageEmbed)
+            applyActionRows(channel.sendMessageEmbeds(List.of(((JDAMessageEmbed) sendable).getMessage())), index).queue();
+        else
+            throw new IllegalArgumentException("Sendable of Book page must be a JDAMessage or JDAMessageEmbed");
     }
 
     /**
@@ -110,7 +107,7 @@ public class JDABook extends Book {
      * @param index  the index of the page
      */
     @CheckReturnValue
-    private MessageAction applyActionRows(@NotNull MessageAction action, int index) {
+    private @NotNull MessageAction applyActionRows(@NotNull MessageAction action, int index) {
         buttons.add(0, previousButton.withDisabled(index == 0).withId(getName() + "-" + --index));
         buttons.add(1, nextButton.withDisabled(index == pages.size() - 1).withId(getName() + "-" + ++index));
         ActionRow bookActionRow = ActionRow.of(buttons);
