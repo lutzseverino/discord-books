f56ebd400f62af06fb2a7773de4e01d6b9ef1a13
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-04-10 14:19:25 +0200

    Remove assert and noinspection rule

--- jda/src/main/java/st/networkers/discordbooks/jda/listener/ButtonListener.java
+++ jda/src/main/java/st/networkers/discordbooks/jda/listener/ButtonListener.java
@@ -1,6 +1,7 @@
 package st.networkers.discordbooks.jda.listener;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Message.Interaction;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
@@ -9,6 +10,7 @@ import st.networkers.discordbooks.book.Book;
 import st.networkers.discordbooks.cache.Cache;
 import st.networkers.discordbooks.jda.book.JDABook;
 import st.networkers.discordbooks.jda.errors.JDABookErrorHandler;
+import st.networkers.discordbooks.jda.errors.JDABookErrorHandlerImpl;
 
 public class ButtonListener extends ListenerAdapter {
     private final Cache<? extends Book> books;
@@ -19,21 +21,22 @@ public class ButtonListener extends ListenerAdapter {
         this.errorHandler = errorHandler;
     }
 
+    public ButtonListener(Cache<? extends Book> books) {
+        this(books, new JDABookErrorHandlerImpl());
+    }
+
     @Override public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
         if (event.getButton().getStyle() != ButtonStyle.LINK) {
             String id = event.getButton().getId();
 
-            // Button ID can't be null because of the ButtonStyle.LINK check
-            assert id != null;
-
+            //noinspection ConstantConditions
             String[] split = id.split("-");
             String bookId = split[0];
             int pageToSet = Integer.parseInt(split[1]);
             JDABook book = (JDABook) books.get(bookId);
 
             Message message = event.getMessage();
-            Message.Interaction interaction = message.getInteraction();
-
+            Interaction interaction = message.getInteraction();
 
             if (book != null) {
                 if (interaction != null && interaction.getUser() == event.getUser() && !book.isNavigationPublic()) {
