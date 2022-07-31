1998acc442cce1aaf50f627402c3928e01a99b09
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-07-31 17:24:31 +0200

    Create abstract layer of message actions
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- core/src/main/java/st/networkers/discordbooks/message/Sendable.java
+++ core/src/main/java/st/networkers/discordbooks/message/Sendable.java
@@ -7,8 +7,52 @@ public interface Sendable {
 
     Optional<String> getText();
 
-    Sendable setEmbeds(Embed... embeds);
-
     List<Embed> getEmbeds();
+
+    List<List<Actionable>> getActionables();
+
+    interface Actionable {
+
+        Optional<String> getId();
+
+        boolean isDisabled();
+
+        interface Clickable {
+
+            Style getStyle();
+
+            String getDisplay();
+
+            Optional<String> getUrl();
+
+            Optional<String> getEmoji();
+
+            enum Style {
+                PRIMARY,
+                SECONDARY,
+                SUCCESS,
+                DANGER,
+                LINK,
+                UNKNOWN,
+            }
+        }
+
+        interface Selectable {
+
+            List<Selectable.Option> getOptions();
+
+            interface Option {
+                String getDisplay();
+
+                String getValue();
+
+                String getDescription();
+
+                boolean isDefault();
+
+                Optional<String> getEmoji();
+            }
+        }
+    }
 
 }
