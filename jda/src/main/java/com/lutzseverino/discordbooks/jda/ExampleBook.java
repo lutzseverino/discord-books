c7cba3bb5e2e6b9ade056a295d51c9859d0a9588
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-08-03 17:35:05 +0200

    style: update example book to match readme
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

---
+++ jda/src/main/java/com/lutzseverino/discordbooks/jda/ExampleBook.java
@@ -0,0 +1,59 @@
+package com.lutzseverino.discordbooks.jda;
+
+import com.lutzseverino.discordbooks.book.Book;
+import com.lutzseverino.discordbooks.discord.component.ActionableRow;
+import com.lutzseverino.discordbooks.discord.component.Clickable;
+import com.lutzseverino.discordbooks.discord.component.impl.ClickableImpl;
+import com.lutzseverino.discordbooks.discord.component.impl.SelectableImpl;
+import com.lutzseverino.discordbooks.discord.message.impl.EmbedImpl;
+import com.lutzseverino.discordbooks.discord.message.impl.SendableImpl;
+
+public class ExampleBook extends Book {
+
+    public ExampleBook() {
+        super("example");
+
+        addPages(
+                new SendableImpl("A regular ol' text message."),
+                new SendableImpl().addEmbeds(new EmbedImpl()
+                                .setTitle("Embeds?")
+                                .setDescription("No problemo."),
+                        new EmbedImpl()
+                                .setTitle("Another one, too!")
+                                .setDescription("Messages can have multiple of these!")),
+                new SendableImpl().setText("Text and embeds at the same time?")
+                        .addEmbeds(
+                                new EmbedImpl()
+                                        .setTitle("I betcha")
+                                        .setDescription("Need more convincing? Too bad, I've got no more pages left."))
+        );
+
+        setNextClickable(new ClickableImpl(Clickable.Style.PRIMARY).setEmoji("➡"));
+        setPreviousClickable(new ClickableImpl(Clickable.Style.PRIMARY).setEmoji("⬅️"));
+
+        setClickables(
+                getPreviousClickable(),
+                new ClickableImpl(Clickable.Style.DANGER)
+                        .setId("example-danger")
+                        .setEmoji("🔥"),
+                getNextClickable()
+        );
+
+        setActionableRows(
+                getClickableRow(),
+                ActionableRow.of(
+                        new ClickableImpl(Clickable.Style.SECONDARY)
+                                .setId("example-secondary")
+                                .setDisplay("More rows?"),
+                        new ClickableImpl(Clickable.Style.SUCCESS)
+                                .setId("example-success")
+                                .setDisplay("Easy task!")
+                ),
+                ActionableRow.of(
+                        new SelectableImpl("select", new SelectableImpl.OptionImpl("Menus too!", "Amazing")
+                                .setDefault(true)
+                        )
+                )
+        );
+    }
+}
