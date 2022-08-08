c6ecdb78eacc8e2966d710db196393fc7ce39039
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-08-08 02:27:22 +0200

    style(jda): rewrite error messages and set ephemeral
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- jda/src/main/java/com/lutzseverino/discordbooks/jda/errors/JDABookErrorHandlerImpl.java
+++ jda/src/main/java/com/lutzseverino/discordbooks/jda/errors/JDABookErrorHandlerImpl.java
@@ -4,10 +4,10 @@ import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
 public class JDABookErrorHandlerImpl implements JDABookErrorHandler {
     public void whenBookIsNull(ButtonInteractionEvent event) {
-        event.reply("Book not found, it may no longer exist in the codebase.").queue();
+        event.reply("This book may no longer exist.").setEphemeral(true).queue();
     }
 
     public void whenUserIsNotOwner(ButtonInteractionEvent event) {
-        event.reply("You are not the author of this book.").queue();
+        event.reply("You cannot navigate this book").setEphemeral(true).queue();
     }
 }
