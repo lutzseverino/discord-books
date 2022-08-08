cb645e02de024fd80c77d2013d64a6e155e7848b
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-08-08 02:14:05 +0200

    feat(core): add ActionableRow#of method for lists
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- core/src/main/java/com/lutzseverino/discordbooks/discord/component/ActionableRow.java
+++ core/src/main/java/com/lutzseverino/discordbooks/discord/component/ActionableRow.java
@@ -1,16 +1,23 @@
 package com.lutzseverino.discordbooks.discord.component;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class ActionableRow {
-    private final List<? extends Actionable> actionables;
+    private final List<Actionable> actionables = new ArrayList<>();
 
-    public ActionableRow(List<? extends Actionable> actionables) {
-        this.actionables = actionables;
+    public ActionableRow(@JsonProperty("actionables") List<? extends Actionable> actionables) {
+        this.actionables.addAll(actionables);
     }
 
     public static ActionableRow of(Actionable... actionables) {
-        return new ActionableRow(List.of(actionables));
+        return of(List.of(actionables));
+    }
+
+    public static ActionableRow of(List<? extends Actionable> actionables) {
+        return new ActionableRow(actionables);
     }
 
     public List<? extends Actionable> getActionables() {
