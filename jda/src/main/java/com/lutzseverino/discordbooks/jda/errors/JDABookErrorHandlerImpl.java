30a442334b750d17b64a71c60e37263e1929dff6
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-08-08 16:31:59 +0200

    fix(jda): add override annotation to error handler impl
    Signed-off-by: Jasper Lutz Severino <jasperlutzseverino@gmail.com>

--- jda/src/main/java/com/lutzseverino/discordbooks/jda/errors/JDABookErrorHandlerImpl.java
+++ jda/src/main/java/com/lutzseverino/discordbooks/jda/errors/JDABookErrorHandlerImpl.java
@@ -3,11 +3,11 @@ package com.lutzseverino.discordbooks.jda.errors;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 
 public class JDABookErrorHandlerImpl implements JDABookErrorHandler {
-    public void whenBookIsNull(ButtonInteractionEvent event) {
+    @Override public void whenBookIsNull(ButtonInteractionEvent event) {
         event.reply("This book may no longer exist.").setEphemeral(true).queue();
     }
 
-    public void whenUserIsNotOwner(ButtonInteractionEvent event) {
+    @Override public void whenUserIsNotOwner(ButtonInteractionEvent event) {
         event.reply("You cannot navigate this book").setEphemeral(true).queue();
     }
 }
